d8b84ab43a3c889b61fd68bcbe08473e6b7a4838
zzingmaru <117323698+zzingmaru@users.noreply.github.com>
2023-01-17 15:12:52 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 204 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		
+		scan.close();
+		
+		System.out.println((A>B) ? ">" : ((A<B) ? "<" : "=="));
+	}
+}
